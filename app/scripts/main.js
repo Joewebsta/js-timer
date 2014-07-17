7fa59d13c56bc7077287f511955c01e688b492ff
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-17 19:18:25 -0400

    Intial pass at timer js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+  var body = document.getElementsByTagName("body")[0],
+      timerForm = document.getElementsByClassName("js-timer-form")[0];
+
+
+  timerForm.onsubmit = function(e) {
+    e.preventDefault();
+
+    var timerFormValue = document.getElementById("time").value;
+
+    body.innerHTML = timerFormValue;
+
+     var hello = setInterval(function(){
+      timerFormValue--
+      
+      body.innerHTML = timerFormValue;
+
+      if (timerFormValue === 0) {
+        clearInterval(hello);
+      }      
+
+    }, 1000);
+  }
+});
